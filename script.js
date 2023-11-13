0ff974a9cc1b6d8494d9a511896b1bf585f9e441
Abdullah <abdullah.graphitive@gmail.com>
2023-11-13 04:17:51 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,79 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//   const image = document.querySelector(".imageMouseOver");
+
+//   image.addEventListener("mouseenter", function () {
+//     // Change the image source when the mouse enters
+//     image.src = "img/imgblue.png";
+//   });
+
+//   image.addEventListener("mouseleave", function () {
+//     // Change back to the original image source when the mouse leaves
+//     image.src = "img/imgblack.png";
+//   });
+// });
+const imageTwo = document.querySelector(".card2-Function");
+const image = document.querySelector(".imageMouseOver");
+const imageThree = document.querySelector(".card3-Function");
+const imageFour = document.querySelector(".card4-Function");
+const imageFive = document.querySelector(".card5-Function");
+const imageSix = document.querySelector(".card6-Function");
+const imageSeven = document.querySelector(".card7-Function");
+const imageEight = document.querySelector(".card8-Function");
+const imageNine = document.querySelector(".card9-Function");
+image.addEventListener("mouseenter", function () {
+  // Change the image source when the mouse enters
+  image.src = "img/imgblue.png";
+});
+
+image.addEventListener("mouseleave", function () {
+  // Change back to the original image source when the mouse leaves
+  image.src = "img/imgblack.png";
+});
+imageTwo.addEventListener("mouseenter", function () {
+  imageTwo.src = "img/imag1blue.png";
+});
+imageTwo.addEventListener("mouseleave", function () {
+  imageTwo.src = "img/imag1black.png";
+});
+imageThree.addEventListener("mouseenter", function () {
+  imageThree.src = "img/imgblue.png";
+});
+imageThree.addEventListener("mouseleave", function () {
+  imageThree.src = "img/imgblack.png";
+});
+imageFour.addEventListener("mouseenter", function () {
+  imageFour.src = "img/imgblue.png";
+});
+imageFour.addEventListener("mouseleave", function () {
+  imageFour.src = "img/imgblack.png";
+});
+imageFive.addEventListener("mouseenter", function () {
+  imageFive.src = "img/image3blue.png";
+});
+imageFive.addEventListener("mouseleave", function () {
+  imageFive.src = "img/image3black.png";
+});
+imageSix.addEventListener("mouseenter", function () {
+  imageSix.src = "img/image4blue.png";
+});
+imageSix.addEventListener("mouseleave", function () {
+  imageSix.src = "img/image4black.png";
+});
+imageSeven.addEventListener("mouseenter", function () {
+  imageSeven.src = "img/Londonimgblue.png";
+});
+imageSeven.addEventListener("mouseleave", function () {
+  imageSeven.src = "img/Londonimgblack.png";
+});
+imageEight.addEventListener("mouseenter", function () {
+  imageEight.src = "img/torontoBlue.png";
+});
+imageEight.addEventListener("mouseleave", function () {
+  imageEight.src = "img/torontoBlack.png";
+});
+imageNine.addEventListener("mouseenter", function () {
+  imageNine.src = "img/jerseyBlue.png";
+});
+imageNine.addEventListener("mouseleave", function () {
+  imageNine.src = "img/jerseyBack.png";
+});
